7653736ef411f5b4fbe7a6127f2ecbd16537fe2f
anand <anandnambakam@gmail.com>
2021-12-24 02:30:01 -0800

    [server] Run MySQL migrations on server instantiation
    Summary:
    We want the MySQL migrations to run on server instantiation. We only want them run once, so it's best in the parent process before `cluster.fork()`. Use `process.env.NODE_UNIQUE_ID` instead of `cluster.isMaster` for the entry point module. I'm still working on exactly why this is needed, because `cluster.isMaster` is determined by `process.env.NODE_UNIQUE_ID`. I'm stubbing this out for now, and will expand on it later with more detail.
    
    I also think that ideally `server.js` were refactored a little, but not sure if that's priority. I'm making a Notion doc to provide much more context for both of these claims.
    
    Test Plan: Need to do more testing, but have checked the same test cases as in `migrations.js`.
    
    Reviewers: atul
    
    Subscribers: ashoat, palys-swm, Adrian, karol-bisztyga, boristopalov
    
    Differential Revision: https://phabricator.ashoat.com/D2722

--- server/src/server.js
+++ server/src/server.js
@@ -7,6 +7,7 @@ import expressWs from 'express-ws';
 import os from 'os';
 
 import './cron/cron';
+import { migrate } from './database/migrations';
 import { jsonEndpoints } from './endpoints';
 import { emailSubscriptionResponder } from './responders/comm-landing-responders';
 import {
@@ -34,7 +35,19 @@ import { getGlobalURLFacts, getLandingURLFacts } from './utils/urls';
 const { baseRoutePath } = getGlobalURLFacts();
 const landingBaseRoutePath = getLandingURLFacts().baseRoutePath;
 
+async function doMigrations() {
+  const ret: boolean = await migrate();
+  return { ret };
+}
+
 if (cluster.isMaster) {
+  (async () => {
+    const didMigrationsSucceed = await doMigrations();
+    if (didMigrationsSucceed.ret === false) {
+      // Force nodemon to exit as well
+      process.exit(2);
+    }
+  })();
   const cpuCount = os.cpus().length;
   for (let i = 0; i < cpuCount; i++) {
     cluster.fork();
