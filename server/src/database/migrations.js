80800e4da50456bcce47372ae193899e8c350c21
anand <anandnambakam@gmail.com>
2022-01-04 11:52:48 -0800

    [server] Adding `migrations.js`
    Summary:
    This is the automated MySQL migrations file, which is to be run when server is spun up by `server.js`.
    
    I realize that this file is in the wrong place (I think it belongs in `src/database/`, not `src/scripts/`). I didn't want to move it during review, but will do after.
    
    Test Plan:
    Here are the tests I've run so far:
    
    Tests run so far:
    1. migrations are valid, exits gracefully
    2. `db_version` >= migration index (in other words, the `db_version` is greater than the current migration key)
        1. `continue;`'s until a valid migration index is reached. Exits gracefully if such index exists, and also if it doesn't
    3. `db_version` >= all migration indices (`db_version` is higher than all referenced migrations)
        1. Exits gracefully
    4. `db_version` is not set in `metadata` table
        1. `db_version` is initialized to 0 in `metadata` table. Function continues to one of cases 1 to 3
    5. `db_version` is already set in `metadata` table
        1. `db_version` is not re-initialized to 0. Function continues to one of cases 1 to 3
    6. migration failure, does transaction rollback?
        1. yes
    7. migration success, does transaction commit?
        1. yes
    
    Reviewers: atul
    
    Subscribers: ashoat, palys-swm, Adrian, karol-bisztyga, benschac, boristopalov
    
    Differential Revision: https://phabricator.ashoat.com/D2628

---
+++ server/src/database/migrations.js
@@ -0,0 +1,93 @@
+// @flow
+
+import type { QueryResults } from 'mysql';
+
+import { dbQuery, SQL } from './database';
+
+const migrations: $ReadOnlyMap<number, () => Promise<void>> = new Map();
+
+async function migrate(): Promise<boolean> {
+  let dbVersion = null;
+  try {
+    await createDBVersionEntry();
+    dbVersion = await getDBVersion();
+    console.log(`(node:${process.pid}) DB version: ${dbVersion}`);
+  } catch (err) {
+    console.error(`(node:${process.pid}) ${err}`);
+    return false;
+  }
+
+  for (const [idx, migration] of migrations.entries()) {
+    if (idx <= dbVersion) {
+      continue;
+    }
+
+    try {
+      await startTransaction();
+      await migration();
+      await updateDBVersion(idx);
+      await commitTransaction();
+      console.log(`(node:${process.pid}) migration ${idx} succeeded.`);
+    } catch (err) {
+      console.error(`(node:${process.pid}) migration ${idx} failed.`);
+      console.error(err);
+      await rollbackTransaction();
+      return false;
+    }
+  }
+  return true;
+}
+
+async function createDBVersionEntry(): Promise<QueryResults> {
+  const insertQuery = SQL`
+    INSERT INTO metadata (name, data)
+    VALUES ('db_version', 0)
+    ON DUPLICATE KEY
+    UPDATE
+      name = name;
+  `;
+  return dbQuery(insertQuery);
+}
+
+async function getDBVersion(): Promise<number> {
+  const versionQuery = SQL`
+    SELECT data
+    FROM metadata
+    WHERE name = 'db_version';
+  `;
+  const [[versionResult]] = await dbQuery(versionQuery);
+  const dbVersion = versionResult.data;
+  return dbVersion;
+}
+
+async function updateDBVersion(dbVersion: number): Promise<QueryResults> {
+  const updateQuery = SQL`
+    UPDATE metadata
+    SET data = ${dbVersion}
+    WHERE name = 'db_version';
+  `;
+  return dbQuery(updateQuery);
+}
+
+async function startTransaction(): Promise<QueryResults> {
+  const beginTxnQuery = SQL`
+        START TRANSACTION;
+      `;
+  return dbQuery(beginTxnQuery);
+}
+
+async function commitTransaction(): Promise<QueryResults> {
+  const endTxnQuery = SQL`
+        COMMIT;
+      `;
+  return dbQuery(endTxnQuery);
+}
+
+async function rollbackTransaction(): Promise<QueryResults> {
+  const rollbackTxnQuery = SQL`
+        ROLLBACK;
+      `;
+  return dbQuery(rollbackTxnQuery);
+}
+
+export default migrate;
