d13396b4b746437d8ff76ad3800bcd60446dbd6f
Benjamin Schachter <benschac@gmail.com>
2022-01-12 12:11:07 -0500

    [web] [2/n] [darktheme] import colors
    Summary: Imports the color swatch into the web app
    
    Test Plan: look in the console, you should see the colors loaded in dev tools <will add screen shot shortly>
    
    Reviewers: atul
    
    Subscribers: ashoat, palys-swm, Adrian, karol-bisztyga
    
    Differential Revision: https://phabricator.ashoat.com/D2828

--- web/app.react.js
+++ web/app.react.js
@@ -2,6 +2,7 @@
 
 import '@fontsource/inter';
 import 'basscss/css/basscss.min.css';
+import './theme.css';
 import { config as faConfig } from '@fortawesome/fontawesome-svg-core';
 import { faCalendar, faComments } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
