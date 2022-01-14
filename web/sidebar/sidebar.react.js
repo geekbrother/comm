9506a34afdbac98f2b83bad4991e8a56866efb4b
Benjamin Schachter <benschac@gmail.com>
2022-01-14 13:23:03 -0500

    [web] [5/n] [layout] add spaces component
    Summary: Add section for community and inbox selection
    
    Test Plan: should look like screen shot
    
    Reviewers: atul
    
    Subscribers:

--- web/sidebar/sidebar.react.js
+++ web/sidebar/sidebar.react.js
@@ -4,10 +4,12 @@ import * as React from 'react';
 
 import AppSwitcher from './app-switcher.react';
 import css from './sidebar.css';
+import Spaces from './spaces.react';
 
 function SideBar(): React.Node {
   return (
     <aside className={css.container}>
+      <Spaces />
       <AppSwitcher />
     </aside>
   );
---
+++ web/sidebar/spaces.react.js
@@ -0,0 +1,11 @@
+// @flow
+
+import * as React from 'react';
+
+import css from './spaces.css';
+
+function Spaces(): React.Node {
+  return <div className={css.container}></div>;
+}
+
+export default Spaces;
