37d2c35a321e8a4494d630ce495d76ac0451eb99
karol-bisztyga <karol.bisztyga@swmansion.com>
2022-01-18 14:16:19 +0100

    [8/n][Services] Tunnelbroker - Database Manager - public functions
    Summary:
    
    Full stack:
    [tbh]
    
    Test Plan:
    
    Bult stack uses the docker-compose build tunnelbroker-server and runs the server using the docker-compose run tunnelbroker-server command.
    
    Reviewers:
    
    Subscribers:

--- services/tunnelbroker/docker-server/contents/server/src/Database/DatabaseManager.cpp
+++ services/tunnelbroker/docker-server/contents/server/src/Database/DatabaseManager.cpp
@@ -19,6 +19,47 @@ void DatabaseManager::innerPutItem(
   }
 }
 
+void DatabaseManager::putSessionItem(const DeviceSessionItem &item) {
+  Aws::DynamoDB::Model::PutItemRequest request;
+  request.SetTableName(item.getTableName());
+  request.AddItem(
+      DeviceSessionItem::FIELD_SESSION_ID,
+      Aws::DynamoDB::Model::AttributeValue(item.getSessionId()));
+  request.AddItem(
+      DeviceSessionItem::FIELD_DEVICE_ID,
+      Aws::DynamoDB::Model::AttributeValue(item.getDeviceId()));
+  request.AddItem(
+      DeviceSessionItem::FIELD_PUBKEY,
+      Aws::DynamoDB::Model::AttributeValue(item.getPubKey()));
+  request.AddItem(
+      DeviceSessionItem::FIELD_NOTIFY_TOKEN,
+      Aws::DynamoDB::Model::AttributeValue(item.getNotifyToken()));
+  request.AddItem(
+      DeviceSessionItem::FIELD_DEVICE_TYPE,
+      Aws::DynamoDB::Model::AttributeValue(item.getDeviceType()));
+  request.AddItem(
+      DeviceSessionItem::FIELD_APP_VERSION,
+      Aws::DynamoDB::Model::AttributeValue(item.getAppVersion()));
+  request.AddItem(
+      DeviceSessionItem::FIELD_DEVICE_OS,
+      Aws::DynamoDB::Model::AttributeValue(item.getDeviceOs()));
+  request.AddItem(
+      DeviceSessionItem::FIELD_CHECKPOINT_TIME,
+      Aws::DynamoDB::Model::AttributeValue(
+          std::to_string(item.getCheckpointTime())));
+
+  this->innerPutItem(std::make_shared<DeviceSessionItem>(item), request);
+}
+
+std::shared_ptr<DeviceSessionItem>
+DatabaseManager::findSessionItem(const std::string &sessionId) {
+  Aws::DynamoDB::Model::GetItemRequest request;
+  request.AddKey(
+      DeviceSessionItem::FIELD_SESSION_ID,
+      Aws::DynamoDB::Model::AttributeValue(sessionId));
+  return std::move(this->innerFindItem<DeviceSessionItem>(request));
+}
+
 } // namespace database
 } // namespace network
 } // namespace comm
