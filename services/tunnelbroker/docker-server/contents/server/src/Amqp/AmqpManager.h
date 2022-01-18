4e78c569beccd0d537138cdd527bd5cfd450b3d5
karol-bisztyga <karol.bisztyga@swmansion.com>
2022-01-18 13:12:56 +0100

    [10/n][Services] Tunnelbroker - AMQP manager
    Summary:
    Full stack:
    [tbh]
    
    Test Plan: Bult uses the docker-compose build tunnelbroker-server and runs the server using the docker-compose run tunnelbroker-server command.
    
    Subscribers: ashoat, palys-swm, Adrian, atul, karol-bisztyga
    
    Differential Revision: https://phabricator.ashoat.com/D2877

---
+++ services/tunnelbroker/docker-server/contents/server/src/Amqp/AmqpManager.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "Constants.h"
+#include "DeliveryBroker.h"
+
+#include <boost/asio/deadline_timer.hpp>
+#include <boost/asio/io_service.hpp>
+#include <boost/asio/strand.hpp>
+
+#include <amqpcpp.h>
+#include <amqpcpp/libboostasio.h>
+#include <atomic>
+#include <string>
+
+namespace comm {
+namespace network {
+
+static std::unique_ptr<AMQP::TcpChannel> AmqpChannel;
+static std::atomic<bool> AmqpReady;
+
+void AMQPConnect();
+bool AMQPSend(
+    std::string toDeviceID,
+    std::string fromDeviceID,
+    std::string payload);
+
+} // namespace network
+} // namespace comm
