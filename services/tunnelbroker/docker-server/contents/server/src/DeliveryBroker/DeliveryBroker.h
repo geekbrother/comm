5bece097d2e54ea2dbe4e88ef327cd34870cc060
karol-bisztyga <karol.bisztyga@swmansion.com>
2022-01-18 13:15:12 +0100

    [13/n][Services] Tunnelbroker - Delivery broker

---
+++ services/tunnelbroker/docker-server/contents/server/src/DeliveryBroker/DeliveryBroker.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "Constants.h"
+#include "DeliveryBrokerEntites.h"
+
+#include <string>
+#include <vector>
+
+namespace comm {
+namespace network {
+
+void DeliveryBrokerPush(
+    std::string toDeviceID,
+    std::string fromDeviceID,
+    std::string payload);
+std::vector<DeliveryBrokerMessageStruct>
+DeliveryBrokerGet(std::string deviceID);
+void DeliveryBrokerRemove(std::string key);
+void DeliveryBrokerClear();
+
+} // namespace network
+} // namespace comm
