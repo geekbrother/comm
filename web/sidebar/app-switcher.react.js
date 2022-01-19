f940fdc988f3c9360de20b6a596ed49e7b4bb6b4
Benjamin Schachter <benschac@gmail.com>
2022-01-18 21:20:04 -0500

    [web] [navigation] [1/n] move app switching functionality to sidebar
    Summary: Move App switching functionality to sidebar. Styling + unread, and any other functionality will come in additional diffs
    
    Test Plan: click the links on the side bar, the applications should switch from calendar to chat
    
    Reviewers: atul
    
    Subscribers: ashoat, palys-swm, Adrian, karol-bisztyga
    
    Differential Revision: https://phabricator.ashoat.com/D2879

--- web/sidebar/app-switcher.react.js
+++ web/sidebar/app-switcher.react.js
@@ -1,22 +1,93 @@
 // @flow
 
 import * as React from 'react';
+import { useDispatch } from 'react-redux';
 
+import { mostRecentReadThreadSelector } from 'lib/selectors/thread-selectors';
+import type { Dispatch } from 'lib/types/redux-types';
+
+import { useSelector } from '../redux/redux-utils';
+import { type NavInfo, updateNavInfoActionType } from '../types/nav-types';
 import css from './sidebar.css';
 
-function AppSwitcher(): React.Node {
-  return (
-    <div className={css.container}>
-      <ul>
-        <li>
-          <p>Calendar</p>
-        </li>
-        <li>
-          <p>Chat</p>
-        </li>
-      </ul>
-    </div>
-  );
+type BaseProps = {};
+
+type Props = {
+  // Redux State
+  +navInfo: NavInfo,
+  // Redux dispatch functions
+  +dispatch: Dispatch,
+  +mostRecentReadThread: ?string,
+  +activeThreadCurrentlyUnread: boolean,
+};
+
+class AppSwitcher extends React.PureComponent<Props> {
+  onClickCalendar = (event: SyntheticEvent<HTMLAnchorElement>) => {
+    event.preventDefault();
+    this.props.dispatch({
+      type: updateNavInfoActionType,
+      payload: { tab: 'calendar' },
+    });
+  };
+
+  onClickChat = (event: SyntheticEvent<HTMLAnchorElement>) => {
+    event.preventDefault();
+    this.props.dispatch({
+      type: updateNavInfoActionType,
+      payload: {
+        tab: 'chat',
+        activeChatThreadID: this.props.activeThreadCurrentlyUnread
+          ? this.props.mostRecentReadThread
+          : this.props.navInfo.activeChatThreadID,
+      },
+    });
+  };
+
+  render() {
+    return (
+      <div className={css.container}>
+        <ul>
+          <li>
+            <p>
+              <a onClick={this.onClickCalendar}>Calendar</a>
+            </p>
+          </li>
+          <li>
+            <p>
+              <a onClick={this.onClickChat}>Chat</a>
+            </p>
+          </li>
+        </ul>
+      </div>
+    );
+  }
 }
 
-export default AppSwitcher;
+const ConnectedAppSwitcher: React.ComponentType<BaseProps> = React.memo<BaseProps>(
+  function ConnectedAppSwitcher() {
+    const activeChatThreadID = useSelector(
+      state => state.navInfo.activeChatThreadID,
+    );
+    const navInfo = useSelector(state => state.navInfo);
+
+    const mostRecentReadThread = useSelector(mostRecentReadThreadSelector);
+    const activeThreadCurrentlyUnread = useSelector(
+      state =>
+        !activeChatThreadID ||
+        !!state.threadStore.threadInfos[activeChatThreadID]?.currentUser.unread,
+    );
+
+    const dispatch = useDispatch();
+
+    return (
+      <AppSwitcher
+        navInfo={navInfo}
+        dispatch={dispatch}
+        mostRecentReadThread={mostRecentReadThread}
+        activeThreadCurrentlyUnread={activeThreadCurrentlyUnread}
+      />
+    );
+  },
+);
+
+export default ConnectedAppSwitcher;
