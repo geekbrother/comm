ef8ce0eb2be25b006f1dc94285745df55ac039b1
karol-bisztyga <karol.bisztyga@swmansion.com>
2022-01-20 12:40:36 +0100

    [15/n] Tunnelbroker - reconnect AMQP
    Summary: todo
    
    Test Plan: todo
    
    Subscribers: ashoat, palys-swm, Adrian, atul
    
    Differential Revision: https://phabricator.ashoat.com/D2888

--- services/tunnelbroker/docker-server/contents/server/src/Amqp/AmqpManager.cpp
+++ services/tunnelbroker/docker-server/contents/server/src/Amqp/AmqpManager.cpp
@@ -1,14 +1,14 @@
 #include "AmqpManager.h"
 #include "Constants.h"
 #include "DeliveryBroker.h"
+#include "Tools.h"
 
 #include <boost/asio/io_service.hpp>
 
 namespace comm {
 namespace network {
 
-void AMQPConnect() {
-
+void AMQPConnectInternal() {
   std::cout << "AMQP: Connecting to " << AMQP_URI << std::endl;
 
   // Make a connection and channel
@@ -18,7 +18,9 @@ void AMQPConnect() {
   AmqpChannel = std::make_unique<AMQP::TcpChannel>(&connection);
 
   AmqpChannel->onError([&connection](const char *message) {
-    throw std::runtime_error("AMQP: Channel error: " + std::string(message));
+    std::cout << "channel error: " << message << ", will try to reconnect"
+              << std::endl;
+    AmqpReady = false;
   });
 
   // Create a queue for the current tunnelbroker instance by it's ID
@@ -61,6 +63,24 @@ void AMQPConnect() {
             "AMQP: Queue creation error: " + std::string(message));
       });
   BoostIOservice.run();
+}
+
+void AMQPConnect() {
+  while (true) {
+    long long currentTimestamp = getCurrentTimestamp();
+    if (lastConnectionTimestamp &&
+        currentTimestamp - lastConnectionTimestamp <
+            AMQP_SHORTEST_RECONNECTION_ATTEMPT_INTERVAL) {
+      throw std::runtime_error(
+          "AMQP reconnection attempt interval too short, tried to reconnect "
+          "after " +
+          std::to_string(currentTimestamp - lastConnectionTimestamp) +
+          "ms, the shortest allowed interval is " +
+          std::to_string(AMQP_SHORTEST_RECONNECTION_ATTEMPT_INTERVAL) + "ms");
+    }
+    lastConnectionTimestamp = currentTimestamp;
+    AMQPConnectInternal();
+  }
 }
 
 bool AMQPSend(
