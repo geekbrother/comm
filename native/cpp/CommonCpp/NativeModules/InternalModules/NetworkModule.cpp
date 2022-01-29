415626072270b141794c5dc386e8a1214b024ed6
Atul Madhugiri <atulm@berkeley.edu>
2022-01-29 15:05:25 -0500

    [CommCoreModule] Implement `Client::send`
    Summary: Filled out this boilerplate to communicate with dummy gRPC node server
    
    Test Plan:
    Was able to send stuff and see it on the other side
    
    {F9982}
    
    Reviewers: palys-swm, def-au1t, varun, karol-bisztyga, ashoat
    
    Reviewed By: palys-swm
    
    Subscribers: benschac, ashoat, Adrian, karol-bisztyga
    
    Differential Revision: https://phabricator.ashoat.com/D2932

--- native/cpp/CommonCpp/NativeModules/InternalModules/NetworkModule.cpp
+++ native/cpp/CommonCpp/NativeModules/InternalModules/NetworkModule.cpp
@@ -26,4 +26,17 @@ void NetworkModule::sendPong() {
 void NetworkModule::close() {
   this->networkClient.reset();
 }
+
+grpc::Status NetworkModule::send(
+    std::string sessionID,
+    std::string toDeviceID,
+    std::string payload,
+    std::vector<std::string> blobHashes) {
+  if (this->networkClient) {
+    return this->networkClient->send(
+        sessionID, toDeviceID, payload, blobHashes);
+  }
+  return grpc::Status::CANCELLED;
+}
+
 } // namespace comm
