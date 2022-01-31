39d7a6872a2a9a40e3cae70c8584c53f8c7b4896
Benjamin Schachter <benschac@gmail.com>
2022-01-31 08:20:54 -0500

    [web] create an input component
    Summary: create generic input component
    
    Test Plan: N/A its not used anywhere yet
    
    Reviewers: atul, ashoat!
    
    Subscribers: ashoat, palys-swm, Adrian, karol-bisztyga
    
    Differential Revision: https://phabricator.ashoat.com/D2964

---
+++ web/modals/input.react.js
@@ -0,0 +1,32 @@
+// @flow
+
+import * as React from 'react';
+
+type Props = {
+  +type: string,
+  +placeholder: string,
+  +value: string,
+  +onChange: (value: string) => mixed,
+  +disabled: boolean,
+};
+
+function Input(props: Props, ref): React.Node {
+  const { type, placeholder, value, onChange, disabled } = props;
+  return (
+    <input
+      type={type}
+      placeholder={placeholder}
+      value={value}
+      onChange={onChange}
+      disabled={disabled}
+      ref={ref}
+    />
+  );
+}
+
+const ForwardedInput: React.AbstractComponent<
+  Props,
+  HTMLInputElement,
+> = React.forwardRef<Props, HTMLInputElement>(Input);
+
+export default ForwardedInput;
