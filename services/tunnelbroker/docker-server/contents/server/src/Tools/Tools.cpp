564bfb1314ba26ec96ad8fb89274397072d07f6e
Max Kalashnikov <geekmaks@gmail.com>
2022-01-31 07:01:44 +0200

    [19/n] DeviceID validation.
    Summary:
    
    Full stack:
    D2809 D2875 D2752 D2904 D2759 D2760 D2808 D2762 D2876 D2769 D2877 D2807 D2814 D2870 D2888 D2915 D2976
    
    `deviceID` validation by format:
    prefix:suffix
    
    prefix - is one of the list: ks, web, mobile
    suffix - is a random generated 64 character string
    
    P.S. Allow only one key server `ks:256` and reject others.
    
    Test Plan:
    
    Passed a valid string format and got expected true result. Then passed string in wrong format and got a validation error.
    
    Reviewers:
    
    Subscribers:

--- services/tunnelbroker/docker-server/contents/server/src/Tools/Tools.cpp
+++ services/tunnelbroker/docker-server/contents/server/src/Tools/Tools.cpp
@@ -1,5 +1,7 @@
 #include "Tools.h"
+#include "Constants.h"
 
+#include <algorithm>
 #include <chrono>
 
 namespace comm {
@@ -22,6 +24,30 @@ long long getCurrentTimestamp() {
   using namespace std::chrono;
   return duration_cast<milliseconds>(system_clock::now().time_since_epoch())
       .count();
+}
+
+bool validateDeviceId(std::string deviceId) {
+  // We are allow only one keyserver for now
+  if (deviceId == DEVICEID_DEFAULT_KEYSERVER_ID) {
+    return true;
+  }
+  const std::string delimiter = ":";
+  const std::string prefix = deviceId.substr(0, deviceId.find(delimiter));
+  const std::string suffix = deviceId.substr(
+      deviceId.find(delimiter), deviceId.length() - deviceId.find(delimiter));
+  // Reject all key servers except one allowed before
+  if (prefix == "ks") {
+    return false;
+  }
+  std::vector<std::string> deviceTypes{"ks", "mobile", "web"};
+  if (std::find(std::begin(deviceTypes), std::end(deviceTypes), prefix) ==
+      std::end(deviceTypes)) {
+    return false;
+  }
+  if ((suffix.length() - 1) != DEVICEID_CHAR_LENGTH) {
+    return false;
+  }
+  return true;
 }
 
 } // namespace network
--- services/tunnelbroker/docker-server/contents/server/src/Tools/Tools.h
+++ services/tunnelbroker/docker-server/contents/server/src/Tools/Tools.h
@@ -6,8 +6,8 @@ namespace comm {
 namespace network {
 
 std::string generateRandomString(std::size_t length);
-
 long long getCurrentTimestamp();
+bool validateDeviceId(std::string deviceId);
 
 } // namespace network
 } // namespace comm
