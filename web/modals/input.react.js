728f4453d27d4f522f5750e07722c0c2d31397d4
Benjamin Schachter <benschac@gmail.com>
2022-01-31 08:58:34 -0500

    [web] add label to input component
    Summary: add label for both design + accessability https://developer.mozilla.org/en-US/docs/Web/HTML/Element/label
    
    Test Plan: everything should still work since the prop is optional
    
    Reviewers: atul
    
    Subscribers:

--- web/modals/input.react.js
+++ web/modals/input.react.js
@@ -2,25 +2,34 @@
 
 import * as React from 'react';
 
+import css from './input.css';
+
 type Props = {
   +type: string,
   +placeholder: string,
   +value: string,
   +onChange: (value: SyntheticEvent<HTMLInputElement>) => mixed,
   +disabled: boolean,
+  +id?: string,
+  +label?: string,
 };
 
 function Input(props: Props, ref): React.Node {
-  const { type, placeholder, value, onChange, disabled } = props;
+  const { type, placeholder, value, onChange, disabled, id, label } = props;
   return (
-    <input
-      type={type}
-      placeholder={placeholder}
-      value={value}
-      onChange={onChange}
-      disabled={disabled}
-      ref={ref}
-    />
+    <>
+      {label && <label htmlFor={id}>{label}</label>}
+      <input
+        className={css.input}
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+        disabled={disabled}
+        ref={ref}
+        id={id}
+      />
+    </>
   );
 }
 
