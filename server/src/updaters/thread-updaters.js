geekbrother/comm

commit 4b8f982e4c777a29d5d8faa94f05667972a328d5

make leave thread, remove, add thread members idempotent
Summary:
I modified leaveThread, updateThred (add members to thread), and removeMembers server actions in order to make them idempotent.
`removeMembers` function now checks if there is any member in the thread (from the requested) it is able to delete. If no, it does nothing and return empty message.
`leaveThread` no longer throws an error, if user already left the thread. It sends an empty message response instead.
`updateThread` while adding user already existing in the thread, doesn't send additional redundant notification now.

Test Plan:
I tried to add the same person to the thread a few times in the row. The same with deleting. I tested program also for leaving the same thread twice in a row.
Tests were made on iOS virtual device.

Reviewers: palys-swm, ashoat

Reviewed By: ashoat

Subscribers: ashoat, palys-swm, Adrian, atul, karol-bisztyga

Differential Revision: https://phabricator.ashoat.com/D1937

server/src/updaters/thread-updaters.js
@@ -191,14 +191,19 @@ async function removeMembers(
     changeset,
   );
 
-  const messageData = {
-    type: messageTypes.REMOVE_MEMBERS,
-    threadID: request.threadID,
-    creatorID: viewerID,
-    time: Date.now(),
-    removedUserIDs: actualMemberIDs,
-  };
-  const newMessageInfos = await createMessages(viewer, [messageData]);
+  const newMessageInfos = await (async () => {
+    if (actualMemberIDs.length === 0) {
+      return [];
+    }
+    const messageData = {
+      type: messageTypes.REMOVE_MEMBERS,
+      threadID: request.threadID,
+      creatorID: viewerID,
+      time: Date.now(),
+      removedUserIDs: actualMemberIDs,
+    };
+    return await createMessages(viewer, [messageData]);
+  })();
 
   if (hasMinCodeVersion(viewer.platformDetails, 62)) {
     return { updatesResult: { newUpdates: viewerUpdates }, newMessageInfos };
@@ -232,7 +237,23 @@ async function leaveThread(
   ]);
 
   const threadInfo = fetchThreadResult.threadInfos[request.threadID];
-  if (!viewerIsMember(threadInfo) || !hasPermission) {
+
+  if (!viewerIsMember(threadInfo)) {
+    if (hasMinCodeVersion(viewer.platformDetails, 62)) {
+      return {
+        updatesResult: { newUpdates: [] },
+      };
+    }
+    const threadInfos = await fetchThreadInfos(viewer);
+    return {
+      threadInfos,
+      updatesResult: {
+        newUpdates: [],
+      },
+    };
+  }
+
+  if (!hasPermission) {
     throw new ServerError('invalid_parameters');
   }
 
@@ -608,11 +629,15 @@ async function updateThread(
     membershipRows.push(...recalculateMembershipRows);
     relationshipChangeset.addAll(recalculateRelationshipChangeset);
   }
+  let membersGotAdded = false;
   if (addMembersChangeset) {
     const {
       membershipRows: addMembersMembershipRows,
       relationshipChangeset: addMembersRelationshipChangeset,
     } = addMembersChangeset;
+    membersGotAdded = addMembersMembershipRows.some(
+      row => row.operation === 'save' && Number(row.role) > 0,
+    );
     membershipRows.push(...addMembersMembershipRows);
     relationshipChangeset.addAll(addMembersRelationshipChangeset);
   }
@@ -646,7 +671,7 @@ async function updateThread(
         value: newValue,
       });
     }
-    if (newMemberIDs) {
+    if (newMemberIDs && membersGotAdded) {
       messageDatas.push({
         type: messageTypes.ADD_MEMBERS,
         threadID: request.threadID,

commit e286e111c02796d8c6a990e2b735976cde5e2a55

[RN0.66] 31/n: Fix idempotent return for old clients in leaveThread
Summary:
This bug was introduced in D1937. It only applies to very old clients in the idempotent return case of `leaveThread`, so it was probably never tested (which is reasonable).

The bug was caught as part of the Flow upgrade.

Test Plan: Flow

Reviewers: def-au1t, palys-swm, atul

Subscribers:

server/src/updaters/thread-updaters.js
@@ -244,7 +244,7 @@ async function leaveThread(
         updatesResult: { newUpdates: [] },
       };
     }
-    const threadInfos = await fetchThreadInfos(viewer);
+    const { threadInfos } = await fetchThreadInfos(viewer);
     return {
       threadInfos,
       updatesResult: {