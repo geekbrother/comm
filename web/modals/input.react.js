6465bdd5780d5f257902c53139667ee31b54ea0d
Benjamin Schachter <benschac@gmail.com>
2022-01-31 09:29:07 -0500

    [web] style inputs fields to more closely match the figma
    Summary: update input fields to match figma a bit closer. Currently there aren't empty field states in figma making it difficult to implement the full design. <add screen shot>.  Will update spacings, and labels in a follow up diff
    
    Test Plan: render input fields to look like screen shot and match general design of figma https://www.figma.com/file/a1nkbWgbgjRlrOY9LVurTz/Comm-%2F-Desktop-app?node-id=1313%3A99486
    
    Reviewers: atul
    
    Subscribers: ashoat, palys-swm, Adrian, karol-bisztyga
    
    Differential Revision: https://phabricator.ashoat.com/D3035

--- web/modals/input.react.js
+++ web/modals/input.react.js
@@ -8,14 +8,23 @@ type Props = {
   +value: string,
   +onChange: (value: SyntheticEvent<HTMLInputElement>) => mixed,
   +disabled: boolean,
+  +label?: string,
+  +id: string,
 };
 
+import css from './input.css';
+
 function Input(props: Props, ref): React.Node {
-  const { label, id, ...rest } = props;
+  const { label: labelProp, id, ...rest } = props;
+  const label = labelProp && (
+    <label className={css.label} htmlFor={id}>
+      {labelProp}
+    </label>
+  );
   return (
     <>
-      {label && <label htmlFor={id}>{label}</label>}
-      <input id={id} {...rest} ref={ref} />
+      {label}
+      <input className={css.input} id={id} {...rest} ref={ref} />
     </>
   );
 }
