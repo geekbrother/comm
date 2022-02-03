506463a7687f55ee8a5f070be509353fe60d5816
karol-bisztyga <karol.bisztyga@swmansion.com>
2022-02-03 11:46:48 +0100

    [services] Backup - add Database Entities Tools
    Summary: todo
    
    Test Plan: todo
    
    Subscribers: ashoat, palys-swm, Adrian, atul
    
    Differential Revision: https://phabricator.ashoat.com/D3081

---
+++ services/backup/docker-server/contents/server/src/DatabaseEntities/DatabaseEntitiesTools.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "Item.h"
+
+#include <memory>
+#include <type_traits>
+
+namespace comm {
+namespace network {
+namespace database {
+
+/**
+ * Database - Structure:
+ * backup
+ *  userID[PK]            string
+ *  created[SK]           timestamp
+ *  backupID              string
+ *  recoveryData          bytes
+ *  encryptedBackupKey    bytes
+ *  compactionHolder      string
+ *  attachmentHolders     list<string>
+ * log
+ *  backupID[PK]          string
+ *  logID[SK]             string
+ *  persistedInBlob       bool
+ *  value                 bytes
+ *  attachmentHolders     list<string>
+ */
+
+template <typename T> std::shared_ptr<T> createItemByType() {
+  static_assert(std::is_base_of<Item, T>::value, "T must inherit from Item");
+  return std::make_shared<T>();
+}
+
+} // namespace database
+} // namespace network
+} // namespace comm
