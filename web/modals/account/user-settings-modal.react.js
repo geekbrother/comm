075cd69a85fd42fc49dd7d3c26c427f619451cf8
Benjamin Schachter <benschac@gmail.com>
2022-02-03 11:28:41 -0500

    [web] fix layout of buttons in modal
    Summary:
    error message needed to be none to flex buttons as one would expect. Not sure what the end design is for the modal but either way display needed to be hidden.
    
    before: {F11897}
    after: {F11896}
    
    Test Plan: should look like screen shot
    
    Reviewers: atul
    
    Subscribers: ashoat, palys-swm, Adrian, karol-bisztyga
    
    Differential Revision: https://phabricator.ashoat.com/D3062

--- web/modals/account/user-settings-modal.react.js
+++ web/modals/account/user-settings-modal.react.js
@@ -121,6 +121,10 @@ class UserSettingsModal extends React.PureComponent<Props, State> {
   render() {
     const { inputDisabled } = this.props;
     let mainContent = null;
+    const errorMessageCls = classNames(css['modal-form-error'], {
+      [css['error-message-visible']]: this.state.errorMessage,
+    });
+
     if (this.state.currentTabType === 'general') {
       mainContent = (
         <div>
@@ -198,6 +202,11 @@ class UserSettingsModal extends React.PureComponent<Props, State> {
       );
     }
 
+    let errorMsg;
+    if (this.state.errorMessage) {
+      <div className={errorMessageCls}>{this.state.errorMessage}</div>;
+    }
+
     return (
       <Modal name="Edit account" onClose={this.clearModal} size="large">
         <ul className={css['tab-panel']}>
@@ -237,9 +246,7 @@ class UserSettingsModal extends React.PureComponent<Props, State> {
             </div>
             <div className={css['form-footer']}>
               {buttons}
-              <div className={css['modal-form-error']}>
-                {this.state.errorMessage}
-              </div>
+              {errorMsg}
             </div>
           </form>
         </div>
