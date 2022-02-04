3c452c9b097c0c72bda3b9cfa56bb0d19eb26703
Max Kalashnikov <geekmaks@gmail.com>
2022-02-04 04:54:00 +0200

    [services] Tunnelbroker - Handle SessionID collision without erroring, using random device to generate it.
    Summary: This is the fix for the follow-up task.
    
    Test Plan: Tested is the session is created in case we have no collision and when we have a collision (by adding to the database record with same sessionID).
    
    Subscribers: ashoat, palys-swm, Adrian, atul, karol-bisztyga
    
    Differential Revision: https://phabricator.ashoat.com/D3103

--- services/tunnelbroker/docker-server/contents/server/src/Tools/Tools.h
+++ services/tunnelbroker/docker-server/contents/server/src/Tools/Tools.h
@@ -9,6 +9,7 @@ namespace network {
 std::string generateRandomString(std::size_t length);
 long long getCurrentTimestamp();
 bool validateDeviceId(std::string deviceId);
+std::string generateUUID();
 
 } // namespace network
 } // namespace comm
