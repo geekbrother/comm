af0e1962d2e214302e5e2d330ac712adbb8ffba2
Max Kalashnikov <geekmaks@gmail.com>
2022-02-07 03:06:24 +0200

    [services] Tunnelbroker - Requests SessionID format validation.

--- services/tunnelbroker/docker-server/contents/server/src/Tools/Tools.h
+++ services/tunnelbroker/docker-server/contents/server/src/Tools/Tools.h
@@ -10,6 +10,7 @@ std::string generateRandomString(std::size_t length);
 long long getCurrentTimestamp();
 bool validateDeviceId(std::string deviceId);
 std::string generateUUID();
+bool validateSessionId(std::string sessionId);
 
 } // namespace network
 } // namespace comm
