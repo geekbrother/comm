2348c6fb026ab9e1ecf5c9081ab9053a4d18b0d8
Tomasz Palys <tomasz.palys@swmansion.com>
2022-02-08 15:33:20 +0100

    [native] Add sendPong method
    Summary:
    Add a C++ method that can be called from java. This method should be called in a response to PING background notification.
    We're still using an old method here, which will be replaced by send soon, but updating it should be trivial.
    
    Test Plan: Modify notification handling code to call `NetworkModule.sendPong()`, send a notification and check if the C++ code was called
    
    Reviewers: karol-bisztyga
    
    Subscribers: ashoat, Adrian, atul
    
    Differential Revision: https://phabricator.ashoat.com/D3133

--- native/android/app/src/cpp/GlobalNetworkSingletonJNIHelper.cpp
+++ native/android/app/src/cpp/GlobalNetworkSingletonJNIHelper.cpp
@@ -1,6 +1,16 @@
 #include "GlobalNetworkSingletonJNIHelper.h"
+#include "GlobalNetworkSingleton.h"
 
 namespace comm {
+void GlobalNetworkSingletonJNIHelper::sendPong(
+    facebook::jni::alias_ref<GlobalNetworkSingletonJNIHelper> jThis) {
+  GlobalNetworkSingleton::instance.scheduleOrRun(
+      [](NetworkModule &networkModule) { networkModule.sendPong(); });
+}
+
 void GlobalNetworkSingletonJNIHelper::registerNatives() {
+  javaClassStatic()->registerNatives({
+      makeNativeMethod("sendPong", GlobalNetworkSingletonJNIHelper::sendPong),
+  });
 }
 } // namespace comm
