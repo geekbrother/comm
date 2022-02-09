78e2f8ddd538ee68bbefc3a65557dae59f700a4f
Jacek Nitychoruk <jacek.nitychoruk@swmansion.com>
2022-02-09 14:39:06 +0100

    [web] Introduce thread ancestors in top bar
    Summary:
    Introduce thread ancestors list in top thread bar and keyserver owner. I tried to exactly match Figma design.
    Right now, it doesn't show names of threads between source and current thread, but it can be easily introduced if there is a need.
    
    Test Plan: Tested the compoenent on threads with different count of ancestors to confirm it is displayed correctly.
    
    Reviewers: palys-swm
    
    Subscribers: ashoat, Adrian, atul, karol-bisztyga
    
    Differential Revision: https://phabricator.ashoat.com/D3148

--- web/chat/thread-top-bar.react.js
+++ web/chat/thread-top-bar.react.js
@@ -5,6 +5,7 @@ import * as React from 'react';
 import type { ThreadInfo } from 'lib/types/thread-types';
 
 import SWMansionIcon from '../SWMansionIcon.react';
+import ThreadAncestors from './chat-thread-ancestors.react';
 import css from './thread-top-bar.css';
 
 type threadTopBarProps = {
@@ -27,6 +28,7 @@ function ThreadTopBar(props: threadTopBarProps): React.Node {
           style={threadBackgroundColorStyle}
         />
         <div className={css.threadTitle}>{threadInfo.uiName}</div>
+        <ThreadAncestors threadInfo={threadInfo} />
       </div>
       <button className={css.topBarMenu}>
         <SWMansionIcon icon="menu-vertical" size={20} />
