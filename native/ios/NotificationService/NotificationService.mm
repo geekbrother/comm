5e2d6c863e9d0f6f4defc407d81d27f64d559352
Tomasz Palys <tomasz.palys@swmansion.com>
2022-02-09 16:12:17 +0100

    [iOS] Create empty notification service extension
    Summary: Add an empty extension using XCode `new target` template. The service extension is executed when the app is backgrounded or killed. When the app is running, this code is not called.
    
    Test Plan:
    Modify `prepareIOSNotification` in `send.js` by setting `notification.mutableContent = true`. Modify the code in service so that the `body` gets modified.
    Test on a physical device:
    1. Run the app and send a message from other client - the notification should be displayed with the original body.
    2. Background the app and send a message - the modified notification should be displayed.
    3. Kill the app and send a message - the modified notification should be displayed.
    
    Reviewers: karol-bisztyga, atul, jimpo
    
    Subscribers: ashoat, Adrian
    
    Differential Revision: https://phabricator.ashoat.com/D3162

---
+++ native/ios/NotificationService/NotificationService.mm
@@ -0,0 +1,35 @@
+#import "NotificationService.h"
+#import "Logger.h"
+
+@interface NotificationService ()
+
+@property(nonatomic, strong) void (^contentHandler)
+    (UNNotificationContent *contentToDeliver);
+@property(nonatomic, strong) UNMutableNotificationContent *bestAttemptContent;
+
+@end
+
+@implementation NotificationService
+
+- (void)didReceiveNotificationRequest:(UNNotificationRequest *)request
+                   withContentHandler:
+                       (void (^)(UNNotificationContent *_Nonnull))
+                           contentHandler {
+  self.contentHandler = contentHandler;
+  self.bestAttemptContent = [request.content mutableCopy];
+
+  // TODO modify self.bestAttemptContent here
+
+  comm::Logger::log("TEST");
+
+  self.contentHandler(self.bestAttemptContent);
+}
+
+- (void)serviceExtensionTimeWillExpire {
+  // Called just before the extension will be terminated by the system.
+  // Use this as an opportunity to deliver your "best attempt" at modified
+  // content, otherwise the original push payload will be used.
+  self.contentHandler(self.bestAttemptContent);
+}
+
+@end
