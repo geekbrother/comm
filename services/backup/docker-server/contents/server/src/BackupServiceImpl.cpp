c9d609103e10f07fc934b4b7c4e081a562cd003a
karol-bisztyga <karol.bisztyga@swmansion.com>
2022-02-22 10:28:34 +0100

    [services] Backup - Implementation update
    Summary:
    Update the implementation so it uses the base reactor from D3246.
    
    Also, we edit the proto file so we can use the base reactor for one-direction streams.
    
    Depends on D3246
    
    Test Plan:
    ```
    cd services
    yarn run-backup-service
    ```
    
    Reviewers: palys-swm, geekbrother, varun, jimpo
    
    Subscribers: ashoat, palys-swm, Adrian, atul
    
    Differential Revision: https://phabricator.ashoat.com/D3248

--- services/backup/docker-server/contents/server/src/BackupServiceImpl.cpp
+++ services/backup/docker-server/contents/server/src/BackupServiceImpl.cpp
@@ -1,5 +1,8 @@
 #include "BackupServiceImpl.h"
 
+#include "AuthenticationManager.h"
+#include "ReactorBase.h"
+
 #include <aws/core/Aws.h>
 
 namespace comm {
@@ -13,35 +16,103 @@ BackupServiceImpl::~BackupServiceImpl() {
   Aws::ShutdownAPI({});
 }
 
-grpc::Status BackupServiceImpl::CreateNewBackup(
-    grpc::ServerContext *context,
-    grpc::ServerReaderWriter<
-        backup::CreateNewBackupResponse,
-        backup::CreateNewBackupRequest> *stream) {
-  return grpc::Status(grpc::StatusCode::UNIMPLEMENTED, "not implemented yet");
+grpc::ServerBidiReactor<
+    backup::CreateNewBackupRequest,
+    backup::CreateNewBackupResponse> *
+BackupServiceImpl::CreateNewBackup(grpc::CallbackServerContext *context) {
+  class CreateNewBackupReactor : public ReactorBase<
+                                     backup::CreateNewBackupRequest,
+                                     backup::CreateNewBackupResponse> {
+    crypto::AuthenticationManager authenticationManager;
+
+  public:
+    grpc::Status handleRequest(
+        backup::CreateNewBackupRequest request,
+        backup::CreateNewBackupResponse *response) override {
+      if (this->authenticationManager.getState() !=
+              crypto::AuthenticationState::SUCCESS &&
+          !request.has_authenticationrequestdata()) {
+        return grpc::Status(
+            grpc::StatusCode::INTERNAL,
+            "authentication has not been finished properly");
+      }
+      if (this->authenticationManager.getState() ==
+          crypto::AuthenticationState::FAIL) {
+        return grpc::Status(
+            grpc::StatusCode::INTERNAL, "authentication failure");
+      }
+      if (this->authenticationManager.getState() !=
+          crypto::AuthenticationState::SUCCESS) {
+        backup::FullAuthenticationResponseData authResponse =
+            this->authenticationManager.processRequest(
+                request.authenticationrequestdata());
+        // warning: core dumps possible, in such a case, try making the response
+        // a member of CreateNewBackupReactor class
+        response->set_allocated_authenticationresponsedata(&authResponse);
+        return grpc::Status::OK;
+      }
+      // auth successful - receive compaction
+      std::string compactionDataChunk = request.newcompactionchunk();
+      std::cout << "receive compaction " << compactionDataChunk.size()
+                << std::endl;
+      //...
+      return grpc::Status(grpc::StatusCode::UNIMPLEMENTED, "unimplemented");
+    }
+  };
+
+  return new CreateNewBackupReactor();
 }
 
-grpc::Status BackupServiceImpl::SendLog(
-    grpc::ServerContext *context,
-    grpc::ServerReader<backup::SendLogRequest> *reader,
-    google::protobuf::Empty *response) {
-  return grpc::Status(grpc::StatusCode::UNIMPLEMENTED, "not implemented yet");
+grpc::ServerBidiReactor<backup::SendLogRequest, google::protobuf::Empty> *
+BackupServiceImpl::SendLog(grpc::CallbackServerContext *context) {
+  class SendLogReactor
+      : public ReactorBase<backup::SendLogRequest, google::protobuf::Empty> {
+  public:
+    grpc::Status handleRequest(
+        backup::SendLogRequest request,
+        google::protobuf::Empty *response) override {
+      // TODO handle request
+      return grpc::Status(grpc::StatusCode::UNIMPLEMENTED, "unimplemented");
+    }
+  };
+
+  return new SendLogReactor();
 }
 
-grpc::Status BackupServiceImpl::RecoverBackupKey(
-    grpc::ServerContext *context,
-    grpc::ServerReaderWriter<
-        backup::RecoverBackupKeyResponse,
-        backup::RecoverBackupKeyRequest> *stream) {
-  return grpc::Status(grpc::StatusCode::UNIMPLEMENTED, "not implemented yet");
+grpc::ServerBidiReactor<
+    backup::RecoverBackupKeyRequest,
+    backup::RecoverBackupKeyResponse> *
+BackupServiceImpl::RecoverBackupKey(grpc::CallbackServerContext *context) {
+  class RecoverBackupKeyReactor : public ReactorBase<
+                                      backup::RecoverBackupKeyRequest,
+                                      backup::RecoverBackupKeyResponse> {
+  public:
+    grpc::Status handleRequest(
+        backup::RecoverBackupKeyRequest request,
+        backup::RecoverBackupKeyResponse *response) override {
+      // TODO handle request
+      return grpc::Status(grpc::StatusCode::UNIMPLEMENTED, "unimplemented");
+    }
+  };
+
+  return new RecoverBackupKeyReactor();
 }
 
-grpc::Status BackupServiceImpl::PullBackup(
-    grpc::ServerContext *context,
-    grpc::ServerReaderWriter<
-        backup::PullBackupResponse,
-        backup::PullBackupRequest> *stream) {
-  return grpc::Status(grpc::StatusCode::UNIMPLEMENTED, "not implemented yet");
+grpc::ServerBidiReactor<backup::PullBackupRequest, backup::PullBackupResponse> *
+BackupServiceImpl::PullBackup(grpc::CallbackServerContext *context) {
+  class PullBackupReactor : public ReactorBase<
+                                backup::PullBackupRequest,
+                                backup::PullBackupResponse> {
+  public:
+    grpc::Status handleRequest(
+        backup::PullBackupRequest request,
+        backup::PullBackupResponse *response) override {
+      // TODO handle request
+      return grpc::Status(grpc::StatusCode::UNIMPLEMENTED, "unimplemented");
+    }
+  };
+
+  return new PullBackupReactor();
 }
 
 } // namespace network
