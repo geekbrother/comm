31f4426a3bea8b46d52cbcfe6ba065fa441441f4
karol-bisztyga <karol.bisztyga@swmansion.com>
2022-02-22 10:28:34 +0100

    [services] Backup - Implementation update
    Summary:
    Update the implementation so it uses the base reactor from D3246.
    
    Also, we edit the proto file so we can use the base reactor for one-direction streams.
    
    Depends on D3246
    
    Test Plan:
    ```
    cd services
    yarn run-backup-service
    ```
    
    Reviewers: palys-swm, geekbrother, varun, jimpo
    
    Subscribers: ashoat, palys-swm, Adrian, atul
    
    Differential Revision: https://phabricator.ashoat.com/D3248

--- native/cpp/CommonCpp/grpc/_generated/backup.pb.cc
+++ native/cpp/CommonCpp/grpc/_generated/backup.pb.cc
@@ -441,23 +441,23 @@ const char descriptor_table_protodef_backup_2eproto[] PROTOBUF_SECTION_VARIABLE(
   "nticationData\030\001 \001(\0132\'.backup.SimpleAuthe"
   "nticationRequestData\"K\n\022PullBackupRespon"
   "se\022\031\n\017compactionChunk\030\001 \001(\014H\000\022\022\n\010logChun"
-  "k\030\002 \001(\014H\000B\006\n\004data2\320\002\n\rBackupService\022X\n\017C"
+  "k\030\002 \001(\014H\000B\006\n\004data2\322\002\n\rBackupService\022X\n\017C"
   "reateNewBackup\022\036.backup.CreateNewBackupR"
   "equest\032\037.backup.CreateNewBackupResponse\""
-  "\000(\0010\001\022=\n\007SendLog\022\026.backup.SendLogRequest"
-  "\032\026.google.protobuf.Empty\"\000(\001\022[\n\020RecoverB"
-  "ackupKey\022\037.backup.RecoverBackupKeyReques"
-  "t\032 .backup.RecoverBackupKeyResponse\"\000(\0010"
-  "\001\022I\n\nPullBackup\022\031.backup.PullBackupReque"
-  "st\032\032.backup.PullBackupResponse\"\000(\0010\001b\006pr"
-  "oto3"
+  "\000(\0010\001\022\?\n\007SendLog\022\026.backup.SendLogRequest"
+  "\032\026.google.protobuf.Empty\"\000(\0010\001\022[\n\020Recove"
+  "rBackupKey\022\037.backup.RecoverBackupKeyRequ"
+  "est\032 .backup.RecoverBackupKeyResponse\"\000("
+  "\0010\001\022I\n\nPullBackup\022\031.backup.PullBackupReq"
+  "uest\032\032.backup.PullBackupResponse\"\000(\0010\001b\006"
+  "proto3"
   ;
 static const ::PROTOBUF_NAMESPACE_ID::internal::DescriptorTable*const descriptor_table_backup_2eproto_deps[1] = {
   &::descriptor_table_google_2fprotobuf_2fempty_2eproto,
 };
 static ::PROTOBUF_NAMESPACE_ID::internal::once_flag descriptor_table_backup_2eproto_once;
 const ::PROTOBUF_NAMESPACE_ID::internal::DescriptorTable descriptor_table_backup_2eproto = {
-  false, false, 2524, descriptor_table_protodef_backup_2eproto, "backup.proto", 
+  false, false, 2526, descriptor_table_protodef_backup_2eproto, "backup.proto", 
   &descriptor_table_backup_2eproto_once, descriptor_table_backup_2eproto_deps, 1, 16,
   schemas, file_default_instances, TableStruct_backup_2eproto::offsets,
   file_level_metadata_backup_2eproto, file_level_enum_descriptors_backup_2eproto, file_level_service_descriptors_backup_2eproto,
