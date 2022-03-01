4298a0ef9e67346179f08e52d6e37dd6840f9485
Atul Madhugiri <atulm@berkeley.edu>
2022-02-28 21:45:01 -0500

    [web] Misc renaming in `MessageActionButtons`
    Summary: Changed some previously implementation-specific variable names to be more generic
    
    Test Plan: Find and replace
    
    Reviewers: varun, benschac, def-au1t, palys-swm
    
    Subscribers: ashoat, Adrian, karol-bisztyga
    
    Differential Revision: https://phabricator.ashoat.com/D3312

--- web/chat/message-action-buttons.js
+++ web/chat/message-action-buttons.js
@@ -28,7 +28,7 @@ import {
   TooltipTextItem,
 } from './tooltip.react';
 
-const ellipsisIconExcessVerticalWhitespace = 10;
+const messageActionIconExcessVerticalWhitespace = 10;
 
 const openSidebarText = 'Go to sidebar';
 const createSidebarText = 'Create sidebar';
@@ -69,22 +69,24 @@ function MessageActionButtons(props: MessageActionButtonsProps): React.Node {
       const rect = event.currentTarget.getBoundingClientRect();
       const { top, bottom, left, right, width, height } = rect;
 
-      const dotsPosition: ItemAndContainerPositionInfo = {
+      const iconPosition: ItemAndContainerPositionInfo = {
         containerPosition,
         itemPosition: {
           top:
-            top - containerPosition.top + ellipsisIconExcessVerticalWhitespace,
+            top -
+            containerPosition.top +
+            messageActionIconExcessVerticalWhitespace,
           bottom:
             bottom -
             containerPosition.top -
-            ellipsisIconExcessVerticalWhitespace,
+            messageActionIconExcessVerticalWhitespace,
           left: left - containerPosition.left,
           right: right - containerPosition.left,
           width,
-          height: height - ellipsisIconExcessVerticalWhitespace * 2,
+          height: height - messageActionIconExcessVerticalWhitespace * 2,
         },
       };
-      setPointingTo(dotsPosition);
+      setPointingTo(iconPosition);
     },
     [containerPosition, tooltipVisible],
   );
