b66902212057d674b07869eace13bd9b567edc7b
Tomasz Palys <tomasz.palys@swmansion.com>
2022-03-03 18:43:02 +0100

    [web] Make NavigationPanel reusable
    Summary:
    The final diff of `NavigationPanel` refactoring. NavigationPanel receives a list of items and renders them. AppSwitcher provides a list with available apps.
    Depends on D3341
    
    Test Plan: Check if AppSwitcher still works correctly
    
    Reviewers: benschac, atul, def-au1t
    
    Subscribers: ashoat, Adrian, karol-bisztyga
    
    Differential Revision: https://phabricator.ashoat.com/D3341

--- web/sidebar/app-switcher.react.js
+++ web/sidebar/app-switcher.react.js
@@ -1,11 +1,111 @@
 // @flow
 
+import invariant from 'invariant';
 import * as React from 'react';
+import { useDispatch } from 'react-redux';
 
+import {
+  mostRecentReadThreadSelector,
+  unreadCount,
+} from 'lib/selectors/thread-selectors';
+
+import { useSelector } from '../redux/redux-utils';
+import SWMansionIcon from '../SWMansionIcon.react';
+import { updateNavInfoActionType } from '../types/nav-types';
+import css from './left-layout-aside.css';
 import NavigationPanel from './navigation-panel.react';
 
 function AppSwitcher(): React.Node {
-  return <NavigationPanel />;
+  const activeChatThreadID = useSelector(
+    state => state.navInfo.activeChatThreadID,
+  );
+  const mostRecentReadThread = useSelector(mostRecentReadThreadSelector);
+  const activeThreadCurrentlyUnread = useSelector(
+    state =>
+      !activeChatThreadID ||
+      !!state.threadStore.threadInfos[activeChatThreadID]?.currentUser.unread,
+  );
+  const viewerID = useSelector(
+    state => state.currentUserInfo && state.currentUserInfo.id,
+  );
+
+  const dispatch = useDispatch();
+
+  const onClickCalendar = React.useCallback(
+    (event: SyntheticEvent<HTMLAnchorElement>) => {
+      event.preventDefault();
+      dispatch({
+        type: updateNavInfoActionType,
+        payload: { tab: 'calendar' },
+      });
+    },
+    [dispatch],
+  );
+
+  const onClickChat = React.useCallback(
+    (event: SyntheticEvent<HTMLAnchorElement>) => {
+      event.preventDefault();
+      dispatch({
+        type: updateNavInfoActionType,
+        payload: {
+          tab: 'chat',
+          activeChatThreadID: activeThreadCurrentlyUnread
+            ? mostRecentReadThread
+            : activeChatThreadID,
+        },
+      });
+    },
+    [
+      dispatch,
+      activeThreadCurrentlyUnread,
+      mostRecentReadThread,
+      activeChatThreadID,
+    ],
+  );
+
+  const boundUnreadCount = useSelector(unreadCount);
+
+  invariant(viewerID, 'should be set');
+  let chatBadge = null;
+  if (boundUnreadCount > 0) {
+    chatBadge = <div className={css.chatBadge}>{boundUnreadCount}</div>;
+  }
+
+  const chatNavigationItem = React.useMemo(
+    () => (
+      <p>
+        <span className={css.chatIconWrapper}>
+          <SWMansionIcon icon="message-square" size={24} />
+          {chatBadge}
+        </span>
+        <a onClick={onClickChat}>Chat</a>
+      </p>
+    ),
+    [chatBadge, onClickChat],
+  );
+
+  const calendarNavigationItem = React.useMemo(
+    () => (
+      <p>
+        <SWMansionIcon icon="calendar" size={24} />
+        <a onClick={onClickCalendar}>Calendar</a>
+      </p>
+    ),
+    [onClickCalendar],
+  );
+
+  const navigationItems = React.useMemo(
+    () => [
+      { tab: 'chat', link: chatNavigationItem },
+      {
+        tab: 'calendar',
+        link: calendarNavigationItem,
+      },
+    ],
+    [calendarNavigationItem, chatNavigationItem],
+  );
+
+  return <NavigationPanel navigationItems={navigationItems} />;
 }
 
 export default AppSwitcher;
