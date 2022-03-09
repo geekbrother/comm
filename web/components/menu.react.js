b10f5b8ffbf9ba7c8d5784dc2af8c9b9a6ce8dcb
Jacek Nitychoruk <jacek.nitychoruk@swmansion.com>
2022-03-09 13:12:40 +0100

    [web] display `Menu` action items in portal
    Summary:
    Introduce rendering menu items using portal. Thread actions menu should now have correct position.
    As we need to set correct position of the content in portal, I used `getBoundingClientRect` react function,  which calculates position of rendered element.
    The position needs to be updated window resizes.
    Test Plan:
    In web app, click on thread actions menu. It should appear in correct place and in browser's inspector it will be in the portal.
    Reviewers:
    
    Subscribers:

--- web/components/menu.react.js
+++ web/components/menu.react.js
@@ -3,6 +3,7 @@
 import classnames from 'classnames';
 import * as React from 'react';
 
+import { useMenuPortalContext } from '../menu-portal-provider.react';
 import css from './menu.css';
 
 type MenuPosition = 'left' | 'right' | 'bottom';
@@ -14,18 +15,31 @@ type MenuProps = {
   position?: MenuPosition,
   size?: MenuSize,
   light?: boolean,
+  onChange?: boolean => void,
 };
 
 function Menu(props: MenuProps): React.Node {
   const [isOpen, setIsOpen] = React.useState(false);
+  const [buttonPosition, setButtonPosition] = React.useState(null);
 
+  const buttonRef = React.useRef();
+  const { renderInMenuPortal } = useMenuPortalContext();
   const {
     icon,
     children,
     position = 'bottom',
     size = 'medium',
     light = false,
+    onChange,
   } = props;
+
+  const updatePosition = React.useCallback(() => {
+    if (buttonRef.current) {
+      setButtonPosition(buttonRef.current.getBoundingClientRect());
+    }
+  }, []);
+
+  React.useEffect(updatePosition, [updatePosition]);
 
   const closeMenuCallback = React.useCallback(() => {
     document.removeEventListener('click', closeMenuCallback);
@@ -35,6 +49,20 @@ function Menu(props: MenuProps): React.Node {
   }, [isOpen]);
 
   React.useEffect(() => {
+    if (onChange) {
+      onChange(isOpen);
+    }
+  }, [isOpen, onChange]);
+
+  React.useEffect(() => {
+    if (!window) {
+      return undefined;
+    }
+
+    window.addEventListener('resize', updatePosition);
+    return () => window.removeEventListener('resize', updatePosition);
+  });
+  React.useEffect(() => {
     if (!document || !isOpen) {
       return undefined;
     }
@@ -43,8 +71,9 @@ function Menu(props: MenuProps): React.Node {
   }, [closeMenuCallback, isOpen]);
 
   const switchMenuCallback = React.useCallback(() => {
+    updatePosition();
     setIsOpen(isMenuOpen => !isMenuOpen);
-  }, []);
+  }, [updatePosition]);
 
   if (children.length === 0) {
     return null;
@@ -60,20 +89,30 @@ function Menu(props: MenuProps): React.Node {
       [css.menuActionListLight]: light,
     });
 
-    menuActionList = (
-      <div className={css.menuActionListContainer}>
+    const actionListStyle = buttonPosition
+      ? {
+          top: buttonPosition.top,
+          left: buttonPosition.left,
+        }
+      : {};
+    menuActionList = renderInMenuPortal(
+      <div style={actionListStyle} className={css.menuActionListContainer}>
         <div className={menuActionListClasses}>{children}</div>
-      </div>
+      </div>,
     );
   }
 
   return (
-    <div>
-      <button className={css.menuButton} onClick={switchMenuCallback}>
+    <>
+      <button
+        ref={buttonRef}
+        className={css.menuButton}
+        onClick={switchMenuCallback}
+      >
         {icon}
       </button>
       {menuActionList}
-    </div>
+    </>
   );
 }
 
