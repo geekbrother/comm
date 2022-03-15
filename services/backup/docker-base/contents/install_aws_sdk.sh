836db7c7b97edee991c8396579c2e62a17c4725e
Jim Posen <jim.posen@gmail.com>
2022-03-15 13:29:55 -0400

    Rename build dir from _build to build
    Fixed review comment

--- services/backup/docker-base/contents/install_aws_sdk.sh
+++ services/backup/docker-base/contents/install_aws_sdk.sh
@@ -5,11 +5,11 @@ set -e
 cd /tmp
 
 git clone --recurse-submodules -b 1.9.176 --single-branch https://github.com/aws/aws-sdk-cpp
-mkdir aws-sdk-cpp/_build
-pushd aws-sdk-cpp/_build
+mkdir aws-sdk-cpp/build
+pushd aws-sdk-cpp/build
 cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=/usr/local/aws_sdk -DBUILD_ONLY="core;s3;dynamodb"
 make
 make install
 
-popd # aws-sdk-cpp/_build
+popd # aws-sdk-cpp/build
 rm -rf aws-sdk-cpp
