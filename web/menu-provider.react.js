01e2f1218356ac96e145825af997e8cf082f8271
Jacek Nitychoruk <jacek.nitychoruk@swmansion.com>
2022-03-15 12:04:44 +0100

    [web] Introduce portal for displaying actions menu
    Summary:
    As we need to display menu on the top of UI, I created React portal that can handle rendering menu items.
    The diff introduces the context that provides function allowing to render items in portal.
    Rendering in the portal is not used yet. It will be in the following diffs.
    
    Test Plan: No behavior change in web app. Empty portal div should appear in DOM as a child of `react-root` div. The portal can be tested after introducing rendering in one of the following diffs.
    
    Reviewers: palys-swm
    
    Subscribers: ashoat, Adrian, atul, karol-bisztyga, benschac
    
    Differential Revision: https://phabricator.ashoat.com/D3375

---
+++ web/menu-provider.react.js
@@ -0,0 +1,46 @@
+// @flow
+
+import invariant from 'invariant';
+import * as React from 'react';
+
+import css from './menu.css';
+
+type Props = {
+  +children: React.Node,
+};
+type MenuContextType = {
+  +renderMenu: (itemToRender: React.Node) => void,
+};
+
+const MenuContext: React.Context<?MenuContextType> = React.createContext<?MenuContextType>(
+  {
+    renderMenu: () => {},
+  },
+);
+
+function MenuProvider(props: Props): React.Node {
+  const { children } = props;
+  const [menu, setMenu] = React.useState(null);
+
+  const value = React.useMemo(
+    () => ({
+      renderMenu: setMenu,
+    }),
+    [setMenu],
+  );
+  return (
+    <>
+      <MenuContext.Provider value={value}>{children}</MenuContext.Provider>
+      <div className={css.container}>{menu}</div>
+    </>
+  );
+}
+
+function useRenderMenu(): (itemToRender: React.Node) => void {
+  const context = React.useContext(MenuContext);
+  invariant(context, 'MenuContext not found');
+
+  return context.renderMenu;
+}
+
+export { MenuProvider, useRenderMenu };
