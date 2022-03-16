a99298b208eda3574c4d96a690715ba6bc715829
Jacek Nitychoruk <jacek.nitychoruk@swmansion.com>
2022-03-16 17:40:27 +0100

    [web] display `Menu` action items in portal
    Summary:
    Introduce rendering menu items using portal. Thread actions menu should now have correct position.
    As we need to set correct position of the content in portal, I used `getBoundingClientRect` react function,  which calculates position of rendered element.
    The position needs to be updated window resizes.
    
    Test Plan: In web app, click on thread actions menu. It should appear in correct place and in browser's inspector it will be in the portal.
    
    Subscribers: ashoat, palys-swm, Adrian, atul, karol-bisztyga, benschac
    
    Differential Revision: https://phabricator.ashoat.com/D3377

--- web/components/menu.react.js
+++ web/components/menu.react.js
@@ -3,6 +3,7 @@
 import classnames from 'classnames';
 import * as React from 'react';
 
+import { useRenderMenu } from '../menu-provider.react';
 import css from './menu.css';
 
 type MenuVariant = 'thread-actions' | 'member-actions';
@@ -11,57 +12,121 @@ type MenuProps = {
   +icon: React.Node,
   +children?: React.Node,
   +variant?: MenuVariant,
+  +onChange?: boolean => void,
 };
 
 function Menu(props: MenuProps): React.Node {
   const [isOpen, setIsOpen] = React.useState(false);
+  const [buttonPosition, setButtonPosition] = React.useState(null);
 
-  const { icon, children, variant = 'thread-actions' } = props;
+  const buttonRef = React.useRef();
+  const renderMenu = useRenderMenu();
+  const { icon, children, variant = 'thread-actions', onChange } = props;
+
+  const updatePosition = React.useCallback(() => {
+    if (buttonRef.current) {
+      setButtonPosition(buttonRef.current.getBoundingClientRect());
+    }
+  }, []);
+
+  React.useEffect(updatePosition, [updatePosition]);
+
+  const menuActionListClasses = React.useMemo(
+    () =>
+      classnames(css.menuActionList, {
+        [css.menuActionListThreadActions]: variant === 'thread-actions',
+        [css.menuActionListMemberActions]: variant === 'member-actions',
+      }),
+    [variant],
+  );
+
+  const actionListStyle = React.useMemo(
+    () =>
+      buttonPosition
+        ? {
+            top: buttonPosition.top,
+            left: buttonPosition.left,
+          }
+        : {},
+    [buttonPosition],
+  );
+
+  const menuActionList = React.useMemo(() => {
+    return (
+      <div style={actionListStyle} className={css.menuActionListContainer}>
+        <div className={menuActionListClasses}>{children}</div>
+      </div>
+    );
+  }, [actionListStyle, children, menuActionListClasses]);
+
+  React.useEffect(
+    () =>
+      renderMenu(previous => {
+        if (isOpen) {
+          return menuActionList;
+        } else if (previous === menuActionList) {
+          return null;
+        } else {
+          return previous;
+        }
+      }),
+    [isOpen, menuActionList, renderMenu],
+  );
 
   const closeMenuCallback = React.useCallback(() => {
     document.removeEventListener('click', closeMenuCallback);
     if (isOpen) {
       setIsOpen(false);
+      renderMenu(previous => {
+        if (previous === menuActionList) {
+          return null;
+        } else {
+          return previous;
+        }
+      });
     }
-  }, [isOpen]);
+  }, [isOpen, menuActionList, renderMenu]);
+
+  React.useEffect(() => {
+    onChange?.(isOpen);
+  }, [isOpen, onChange]);
+
+  React.useEffect(() => {
+    if (!window) {
+      return undefined;
+    }
+
+    window.addEventListener('resize', updatePosition);
+    return () => window.removeEventListener('resize', updatePosition);
+  }, [updatePosition]);
 
   React.useEffect(() => {
     if (!document || !isOpen) {
       return undefined;
     }
     document.addEventListener('click', closeMenuCallback);
-    return () => document.removeEventListener('click', closeMenuCallback);
+    return closeMenuCallback;
   }, [closeMenuCallback, isOpen]);
 
   const switchMenuCallback = React.useCallback(() => {
+    updatePosition();
     setIsOpen(isMenuOpen => !isMenuOpen);
-  }, []);
+  }, [updatePosition]);
 
   if (React.Children.count === 0) {
     return null;
   }
 
-  let menuActionList = null;
-  if (isOpen) {
-    const menuActionListClasses = classnames(css.menuActionList, {
-      [css.menuActionListThreadActions]: variant === 'thread-actions',
-      [css.menuActionListMemberActions]: variant === 'member-actions',
-    });
-
-    menuActionList = (
-      <div className={css.menuActionListContainer}>
-        <div className={menuActionListClasses}>{children}</div>
-      </div>
-    );
-  }
-
   return (
-    <div>
-      <button className={css.menuButton} onClick={switchMenuCallback}>
+    <>
+      <button
+        ref={buttonRef}
+        className={css.menuButton}
+        onClick={switchMenuCallback}
+      >
         {icon}
       </button>
-      {menuActionList}
-    </div>
+    </>
   );
 }
 
