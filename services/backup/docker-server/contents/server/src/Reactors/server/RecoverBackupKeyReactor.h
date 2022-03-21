139c14530b7a9c7d23a286e4c244d0d19db3c6f9
karol-bisztyga <karol.bisztyga@swmansion.com>
2022-03-21 09:59:29 +0100

    [services] Backup - Add server reactor implementations
    Summary: todo
    
    Test Plan: todo
    
    Subscribers: ashoat, palys-swm, Adrian, atul, benschac
    
    Differential Revision: https://phabricator.ashoat.com/D3467

---
+++ services/backup/docker-server/contents/server/src/Reactors/server/RecoverBackupKeyReactor.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "ServerBidiReactorBase.h"
+
+#include "../_generated/backup.grpc.pb.h"
+#include "../_generated/backup.pb.h"
+
+#include <iostream>
+#include <memory>
+#include <string>
+
+namespace comm {
+namespace network {
+namespace reactor {
+
+class RecoverBackupKeyReactor : public ServerBidiReactorBase<
+                                    backup::RecoverBackupKeyRequest,
+                                    backup::RecoverBackupKeyResponse> {
+public:
+  std::unique_ptr<grpc::Status> handleRequest(
+      backup::RecoverBackupKeyRequest request,
+      backup::RecoverBackupKeyResponse *response);
+};
+
+std::unique_ptr<grpc::Status> RecoverBackupKeyReactor::handleRequest(
+    backup::RecoverBackupKeyRequest request,
+    backup::RecoverBackupKeyResponse *response) { // TODO handle request
+  return std::make_unique<grpc::Status>(
+      grpc::StatusCode::UNIMPLEMENTED, "unimplemented");
+}
+
+} // namespace reactor
+} // namespace network
+} // namespace comm
