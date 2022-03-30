32af7ff03ba93c80aefa580607adca2da04f599b
Varun Dhananjaya <varun.dhananjaya@gmail.com>
2022-03-30 18:48:27 -0400

    [services][identity] grpc scaffolding for the identity service
    Summary:
    added an async runtime and a bare-bones implementation of the identity service defined in identity.proto
    
    Test Plan: ran `cargo build && cargo run`, then used BloomRPC to make calls to the server. got a successful response.
    
    Reviewers: jimpo, karol-bisztyga
    
    Subscribers: ashoat, palys-swm, Adrian, atul, benschac, yayabosh
    
    Differential Revision: https://phabricator.ashoat.com/D3578

--- services/identity/src/main.rs
+++ services/identity/src/main.rs
@@ -1,3 +1,64 @@
-fn main() {
-    println!("Hello, world!");
+use futures_core::Stream;
+use std::pin::Pin;
+use tonic::{transport::Server, Request, Response, Status};
+
+use identity::identity_service_server::{IdentityService, IdentityServiceServer};
+use identity::{
+  LoginRequest, LoginResponse, RegistrationRequest, RegistrationResponse, VerifyUserTokenRequest,
+  VerifyUserTokenResponse,
+};
+
+const IDENTITY_SERVICE_SOCKET_ADDR: &str = "[::]:50051";
+
+pub mod identity {
+  tonic::include_proto!("identity");
+}
+
+#[derive(Debug, Default)]
+pub struct MyIdentityService {}
+
+#[tonic::async_trait]
+impl IdentityService for MyIdentityService {
+  type RegisterUserStream =
+    Pin<Box<dyn Stream<Item = Result<RegistrationResponse, Status>> + Send + 'static>>;
+
+  async fn register_user(
+    &self,
+    request: Request<tonic::Streaming<RegistrationRequest>>,
+  ) -> Result<Response<Self::RegisterUserStream>, Status> {
+    println!("Got a registration request: {:?}", request);
+    unimplemented!()
+  }
+
+  type LoginUserStream =
+    Pin<Box<dyn Stream<Item = Result<LoginResponse, Status>> + Send + 'static>>;
+
+  async fn login_user(
+    &self,
+    request: Request<tonic::Streaming<LoginRequest>>,
+  ) -> Result<Response<Self::LoginUserStream>, Status> {
+    println!("Got a login request: {:?}", request);
+    unimplemented!()
+  }
+
+  async fn verify_user_token(
+    &self,
+    request: Request<VerifyUserTokenRequest>,
+  ) -> Result<Response<VerifyUserTokenResponse>, Status> {
+    println!("Got a lookup request: {:?}", request);
+    unimplemented!()
+  }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+  let addr = IDENTITY_SERVICE_SOCKET_ADDR.parse()?;
+  let identity_service = MyIdentityService::default();
+
+  Server::builder()
+    .add_service(IdentityServiceServer::new(identity_service))
+    .serve(addr)
+    .await?;
+
+  Ok(())
 }
