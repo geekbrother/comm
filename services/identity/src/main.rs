e4f01e8ce95429da8f207db8f806c0919562d970
Varun Dhananjaya <varun.dhananjaya@gmail.com>
2022-03-30 18:48:27 -0400

    [services][identity] grpc scaffolding for the identity service
    Summary:
    added an async runtime and a bare-bones implementation of the identity service defined in identity.proto
    
    Test Plan: ran `cargo build && cargo run`, then used BloomRPC to make calls to the server. got a successful response.
    
    Reviewers: jimpo, karol-bisztyga
    
    Subscribers: ashoat, palys-swm, Adrian, atul, benschac, yayabosh
    
    Differential Revision: https://phabricator.ashoat.com/D3578

--- services/identity/src/main.rs
+++ services/identity/src/main.rs
@@ -1,3 +1,40 @@
-fn main() {
-    println!("Hello, world!");
+use futures_core::Stream;
+use std::pin::Pin;
+use tonic::{transport::Server, Request, Response, Status};
+
+use identity::identity_service_server::{IdentityService, IdentityServiceServer};
+use identity::{AuthenticationRequest, AuthenticationResponse};
+
+pub mod identity {
+  tonic::include_proto!("identity");
+}
+
+#[derive(Debug, Default)]
+pub struct MyIdentityService {}
+
+#[tonic::async_trait]
+impl IdentityService for MyIdentityService {
+  type AuthenticateUserStream =
+    Pin<Box<dyn Stream<Item = Result<AuthenticationResponse, Status>> + Send + 'static>>;
+
+  async fn authenticate_user(
+    &self,
+    request: Request<tonic::Streaming<AuthenticationRequest>>,
+  ) -> Result<Response<Self::AuthenticateUserStream>, Status> {
+    println!("Got a request: {:?}", request);
+    unimplemented!()
+  }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+  let addr = "[::]:50054".parse()?;
+  let identity_service = MyIdentityService::default();
+
+  Server::builder()
+    .add_service(IdentityServiceServer::new(identity_service))
+    .serve(addr)
+    .await?;
+
+  Ok(())
 }
