a60e2bec3082af633eb40c52ca5fd881abd39d89
Jacek Nitychoruk <jacek.nitychoruk@swmansion.com>
2022-04-01 16:31:58 +0200

    [web] Fetch preview messages from non-watched threads in thread subchannels modal
    Summary:
    The diff introduces fetching missing data using `fetchSingleMostRecentMessages` action for thread that user is not memeber of.
    Test Plan:
    After the change, user that is not member of some subchannel should see preview message for this subchannel in subchannels list for parent thread.
    Reviewers:
    palys-swm
    Subscribers:

--- web/modals/threads/subchannels/subchannels-modal.react.js
+++ web/modals/threads/subchannels/subchannels-modal.react.js
@@ -2,10 +2,19 @@
 
 import * as React from 'react';
 
+import {
+  fetchSingleMostRecentMessagesFromThreads,
+  fetchSingleMostRecentMessagesFromThreadsActionTypes,
+} from 'lib/actions/message-actions';
 import { useFilteredChatListData } from 'lib/selectors/chat-selectors';
 import { threadSearchIndex } from 'lib/selectors/nav-selectors';
 import { childThreadInfos } from 'lib/selectors/thread-selectors';
 import { threadIsChannel } from 'lib/shared/thread-utils';
+import threadWatcher from 'lib/shared/thread-watcher';
+import {
+  useServerCall,
+  useDispatchActionPromise,
+} from 'lib/utils/action-utils';
 
 import Search from '../../../components/search.react';
 import { useSelector } from '../../../redux/redux-utils';
@@ -49,6 +58,36 @@ function SubchannelsModal(props: Props): React.Node {
       searchResultsIDs.includes(item.threadInfo.id),
     );
   }, [allSubchannelsChatList, searchIndex, searchText]);
+
+  const threadIDsWithNoMessages = React.useMemo(
+    () =>
+      filteredSubchannelsChatList
+        .filter(item => !item.mostRecentMessageInfo)
+        .map(item => item.threadInfo.id),
+    [filteredSubchannelsChatList],
+  );
+  const dispatchActionPromise = useDispatchActionPromise();
+  const fetchSingleMostRecentMessages = useServerCall(
+    fetchSingleMostRecentMessagesFromThreads,
+  );
+
+  React.useEffect(() => {
+    if (!threadIDsWithNoMessages.length) {
+      return;
+    }
+    threadIDsWithNoMessages.forEach(tID => threadWatcher.watchID(tID));
+    dispatchActionPromise(
+      fetchSingleMostRecentMessagesFromThreadsActionTypes,
+      fetchSingleMostRecentMessages(threadIDsWithNoMessages),
+    );
+    return () => {
+      threadIDsWithNoMessages.forEach(tID => threadWatcher.removeID(tID));
+    };
+  }, [
+    threadIDsWithNoMessages,
+    fetchSingleMostRecentMessages,
+    dispatchActionPromise,
+  ]);
 
   const subchannels = React.useMemo(() => {
     if (!filteredSubchannelsChatList.length) {
