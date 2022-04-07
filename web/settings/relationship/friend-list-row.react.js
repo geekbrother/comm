edcfee723a3499ad92efb0f2630bc03c1f1cbe34
Tomasz Palys <tomasz.palys@swmansion.com>
2022-04-07 16:08:56 +0200

    [web] Display friend list buttons in the modal
    Summary: Display the buttons depending on relationship status.
    Depends on D3676
    
    Test Plan: Render a couple of rows for users with different relationship statuses (friend, request sent, request received) and check if correct buttons are rendered.
    
    Reviewers: benschac, atul, def-au1t

--- web/settings/relationship/friend-list-row.react.js
+++ web/settings/relationship/friend-list-row.react.js
@@ -1,14 +1,42 @@
 // @flow
 
+import classnames from 'classnames';
 import * as React from 'react';
 
+import { userRelationshipStatus } from 'lib/types/relationship-types';
+
+import SWMansionIcon from '../../SWMansionIcon.react';
 import css from './friend-list-row.css';
 import type { UserRowProps } from './user-list.react';
 
 function FriendListRow(props: UserRowProps): React.Node {
   const { userInfo } = props;
+  let buttons = null;
+  if (userInfo.relationshipStatus === userRelationshipStatus.REQUEST_SENT) {
+    buttons = (
+      <div className={classnames([css.button, css.destructive])}>
+        Cancel request
+      </div>
+    );
+  } else if (
+    userInfo.relationshipStatus === userRelationshipStatus.REQUEST_RECEIVED
+  ) {
+    buttons = (
+      <>
+        <div className={css.button}>Accept</div>
+        <div className={classnames([css.button, css.destructive])}>Reject</div>
+      </>
+    );
+  } else if (userInfo.relationshipStatus === userRelationshipStatus.FRIEND) {
+    buttons = <SWMansionIcon icon="edit" size={22} />;
+  }
 
-  return <div className={css.container}>{userInfo.username}</div>;
+  return (
+    <div className={css.container}>
+      {userInfo.username}
+      <div className={css.buttons}>{buttons}</div>
+    </div>
+  );
 }
 
 export default FriendListRow;
