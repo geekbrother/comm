8d1e5dc14699a1b7a7b1454f99717b40999cabb5
Jacek Nitychoruk <jacek.nitychoruk@swmansion.com>
2022-04-08 14:54:32 +0200

    [web] Allow passing `size` parameter into `SearchModal`
    Summary:
    Allow settings modal size when using `SearchModal`. Will be used in following diffs.
    Test Plan:
    All modals should work as before. Can be tested after next diff in stack.
    Reviewers:
    
    Subscribers:

--- web/modals/search-modal.react.js
+++ web/modals/search-modal.react.js
@@ -3,7 +3,7 @@
 import * as React from 'react';
 
 import Search from '../components/search.react';
-import Modal from './modal.react';
+import Modal, { type ModalSize } from './modal.react';
 import css from './search-modal.css';
 
 type Props = {
@@ -11,18 +11,19 @@ type Props = {
   +searchPlaceholder: string,
   +onClose: () => void,
   +children: (searchText: string) => React.Node,
+  +size?: ModalSize,
 };
 
 function SearchModal(props: Props): React.Node {
   const [searchText, setSearchText] = React.useState('');
-  const { name, searchPlaceholder, onClose, children } = props;
+  const { name, searchPlaceholder, onClose, children, size } = props;
   const child = React.useMemo(() => children(searchText), [
     children,
     searchText,
   ]);
 
   return (
-    <Modal name={name} onClose={onClose}>
+    <Modal name={name} onClose={onClose} size={size}>
       <div className={css.container}>
         <Search
           onChangeText={setSearchText}
