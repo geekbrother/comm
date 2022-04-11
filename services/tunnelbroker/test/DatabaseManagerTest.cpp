8638821bb5409da3769327e71c63b1e2acdfc2cd
Max Kalashnikoff <geekmaks@gmail.com>
2022-04-11 16:02:26 +0530

    [services] Tunnelbroker - Database SessionItem tests.
    Summary:
    Database (DynamoDB) item tests. For SessionItem database entity we already have it performs:
     - Check if the table is available,
     - Create and fulfill database item entity,
     - Put into the database table,
     - Find the record using the primary key,
     - Compare record fields of created item and item from database record,
     - Remove the testing record from the database.
    
    Related linear task: [[ https://linear.app/comm/issue/ENG-686 | ENG-686 ]]
    
    Test Plan: Run `yarn test-tunnelbroker-service` all tests are run and succeed.
    
    Subscribers: ashoat, palys-swm, Adrian, atul, karol-bisztyga, benschac, yayabosh
    
    Differential Revision: https://phabricator.ashoat.com/D3686

--- services/tunnelbroker/test/DatabaseManagerTest.cpp
+++ services/tunnelbroker/test/DatabaseManagerTest.cpp
@@ -22,3 +22,31 @@ protected:
     Aws::ShutdownAPI({});
   }
 };
+
+TEST_F(DatabaseManagerTest, TestOperationsOnDeviceSessionItem) {
+  const database::DeviceSessionItem item(
+      tools::generateUUID(),
+      "mobile:" + tools::generateRandomString(DEVICEID_CHAR_LENGTH),
+      tools::generateRandomString(451),
+      tools::generateRandomString(64),
+      tools::generateRandomString(12),
+      tools::generateRandomString(12),
+      tools::generateRandomString(12));
+  EXPECT_EQ(
+      database::DatabaseManager::getInstance().isTableAvailable(
+          item.getTableName()),
+      true);
+  database::DatabaseManager::getInstance().putSessionItem(item);
+  std::shared_ptr<database::DeviceSessionItem> foundItem =
+      database::DatabaseManager::getInstance().findSessionItem(
+          item.getSessionID());
+  EXPECT_NE(foundItem, nullptr);
+  EXPECT_EQ(item.getDeviceID(), foundItem->getDeviceID());
+  EXPECT_EQ(item.getPubKey(), foundItem->getPubKey());
+  EXPECT_EQ(item.getNotifyToken(), foundItem->getNotifyToken());
+  EXPECT_EQ(item.getDeviceType(), foundItem->getDeviceType());
+  EXPECT_EQ(item.getAppVersion(), foundItem->getAppVersion());
+  EXPECT_EQ(item.getDeviceOs(), foundItem->getDeviceOs());
+  database::DatabaseManager::getInstance().removeSessionItem(
+      item.getSessionID());
+}
