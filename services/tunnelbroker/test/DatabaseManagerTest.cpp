e38dfb190d4a61d21ad73e2e375dd183028b3fd3
Max Kalashnikoff <geekmaks@gmail.com>
2022-04-11 16:18:50 +0530

    [services] Tunnelbroker - Database MessageItem tests
    Summary:
    Database (DynamoDB) item tests. For the `MessageItem` database entity we perform:
     - Check if the table is available,
     - Create and fulfill database item entity,
     - Put into the database table,
     - Find the record using the primary key,
     - Compare record fields of created item and item from database record,
     - Remove the testing record from the database.
    
    Related linear task: [[ https://linear.app/comm/issue/ENG-686 | ENG-686 ]]
    
    Test Plan: Run `yarn test-tunnelbroker-service` all tests are run and succeed.
    
    Subscribers: ashoat, palys-swm, Adrian, atul, karol-bisztyga, benschac, yayabosh
    
    Differential Revision: https://phabricator.ashoat.com/D3689

--- services/tunnelbroker/test/DatabaseManagerTest.cpp
+++ services/tunnelbroker/test/DatabaseManagerTest.cpp
@@ -22,3 +22,29 @@ protected:
     Aws::ShutdownAPI({});
   }
 };
+
+TEST_F(DatabaseManagerTest, TestOperationsOnMessageItem) {
+  const database::MessageItem item(
+      tools::generateUUID(),
+      "mobile:" + tools::generateRandomString(DEVICEID_CHAR_LENGTH),
+      "mobile:" + tools::generateRandomString(DEVICEID_CHAR_LENGTH),
+      tools::generateRandomString(256),
+      tools::generateRandomString(256),
+      static_cast<uint64_t>(std::time(0)) + 600);
+  EXPECT_EQ(
+      database::DatabaseManager::getInstance().isTableAvailable(
+          item.getTableName()),
+      true);
+  database::DatabaseManager::getInstance().putMessageItem(item);
+  std::shared_ptr<database::MessageItem> foundItem =
+      database::DatabaseManager::getInstance().findMessageItem(
+          item.getMessageID());
+  EXPECT_NE(foundItem, nullptr);
+  EXPECT_EQ(item.getFromDeviceID(), foundItem->getFromDeviceID());
+  EXPECT_EQ(item.getToDeviceID(), foundItem->getToDeviceID());
+  EXPECT_EQ(item.getPayload(), foundItem->getPayload());
+  EXPECT_EQ(item.getBlobHashes(), foundItem->getBlobHashes());
+  EXPECT_EQ(item.getExpire() == foundItem->getExpire(), true);
+  database::DatabaseManager::getInstance().removeMessageItem(
+      item.getMessageID());
+}
