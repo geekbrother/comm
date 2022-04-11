584ffeb42bf2b0454c3bf1d9edc49c59c747d616
Max Kalashnikoff <geekmaks@gmail.com>
2022-04-11 16:18:50 +0530

    [services] Tunnelbroker - Database MessageItem tests
    Summary:
    
    Database (DynamoDB) item tests. For the `MessageItem` database entity we perform:
     - Check if the table is available,
     - Create and fulfill database item entity,
     - Put into the database table,
     - Find the record using the primary key,
     - Compare record fields of created item and item from database record,
     - Remove the testing record from the database.
    
    Related linear task: [[ https://linear.app/comm/issue/ENG-686 | ENG-686 ]]
    
    Test Plan:
    
    Run `yarn test-tunnelbroker-service` all tests are run and succeed.
    
    Reviewers:
    
    Subscribers:

--- services/tunnelbroker/test/DatabaseManagerTest.cpp
+++ services/tunnelbroker/test/DatabaseManagerTest.cpp
@@ -22,3 +22,49 @@ protected:
     Aws::ShutdownAPI({});
   }
 };
+
+TEST_F(DatabaseManagerTest, TestOperationsOnMessageItem) {
+  const database::MessageItem item(
+      tools::generateUUID(),
+      "mobile:" + tools::generateRandomString(DEVICEID_CHAR_LENGTH),
+      "mobile:" + tools::generateRandomString(DEVICEID_CHAR_LENGTH),
+      tools::generateRandomString(256),
+      tools::generateRandomString(256),
+      static_cast<uint64_t>(std::time(0)) + 600);
+  EXPECT_EQ(
+      database::DatabaseManager::getInstance().isTableAvailable(
+          item.getTableName()),
+      true);
+  database::DatabaseManager::getInstance().putMessageItem(item);
+  std::shared_ptr<database::MessageItem> foundItem =
+      database::DatabaseManager::getInstance().findMessageItem(
+          item.getMessageID());
+  EXPECT_NE(foundItem, nullptr);
+  EXPECT_EQ(
+      memcmp(
+          item.getFromDeviceID().data(),
+          foundItem->getFromDeviceID().data(),
+          item.getFromDeviceID().size()),
+      0);
+  EXPECT_EQ(
+      memcmp(
+          item.getToDeviceID().data(),
+          foundItem->getToDeviceID().data(),
+          item.getToDeviceID().size()),
+      0);
+  EXPECT_EQ(
+      memcmp(
+          item.getPayload().data(),
+          foundItem->getPayload().data(),
+          item.getPayload().size()),
+      0);
+  EXPECT_EQ(
+      memcmp(
+          item.getBlobHashes().data(),
+          foundItem->getBlobHashes().data(),
+          item.getBlobHashes().size()),
+      0);
+  EXPECT_EQ(item.getExpire() == foundItem->getExpire(), true);
+  database::DatabaseManager::getInstance().removeMessageItem(
+      item.getMessageID());
+}
