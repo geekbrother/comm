eb2447e5b6a903ae7b68e5c54f151b730ec9cb6a
Max Kalashnikoff <geekmaks@gmail.com>
2022-04-11 16:02:26 +0530

    [services] Tunnelbroker - Database SessionItem tests.
    Summary:
    
    Database (DynamoDB) item tests. For SessionItem database entity we already have it performs:
     - Check if the table is available,
     - Create and fulfill database item entity,
     - Put into the database table,
     - Find the record using the primary key,
     - Compare record fields of created item and item from database record,
     - Remove the testing record from the database.
    
    Related linear task: [[ https://linear.app/comm/issue/ENG-686 | ENG-686 ]]
    
    Test Plan:
    
    Run `yarn test-tunnelbroker-service` all tests are run and succeed.
    
    Reviewers:
    
    Subscribers:

--- services/tunnelbroker/test/DatabaseManagerTest.cpp
+++ services/tunnelbroker/test/DatabaseManagerTest.cpp
@@ -22,3 +22,61 @@ protected:
     Aws::ShutdownAPI({});
   }
 };
+
+TEST_F(DatabaseManagerTest, TestOperationsOnDeviceSessionItem) {
+  const database::DeviceSessionItem item(
+      tools::generateUUID(),
+      "mobile:" + tools::generateRandomString(DEVICEID_CHAR_LENGTH),
+      tools::generateRandomString(451),
+      tools::generateRandomString(64),
+      tools::generateRandomString(12),
+      tools::generateRandomString(12),
+      tools::generateRandomString(12));
+  EXPECT_EQ(
+      database::DatabaseManager::getInstance().isTableAvailable(
+          item.getTableName()),
+      true);
+  database::DatabaseManager::getInstance().putSessionItem(item);
+  std::shared_ptr<database::DeviceSessionItem> foundItem =
+      database::DatabaseManager::getInstance().findSessionItem(
+          item.getSessionID());
+  EXPECT_NE(foundItem, nullptr);
+  EXPECT_EQ(
+      memcmp(
+          item.getDeviceID().data(),
+          foundItem->getDeviceID().data(),
+          item.getDeviceID().size()),
+      0);
+  EXPECT_EQ(
+      memcmp(
+          item.getPubKey().data(),
+          foundItem->getPubKey().data(),
+          item.getPubKey().size()),
+      0);
+  EXPECT_EQ(
+      memcmp(
+          item.getNotifyToken().data(),
+          foundItem->getNotifyToken().data(),
+          item.getNotifyToken().size()),
+      0);
+  EXPECT_EQ(
+      memcmp(
+          item.getDeviceType().data(),
+          foundItem->getDeviceType().data(),
+          item.getDeviceType().size()),
+      0);
+  EXPECT_EQ(
+      memcmp(
+          item.getAppVersion().data(),
+          foundItem->getAppVersion().data(),
+          item.getAppVersion().size()),
+      0);
+  EXPECT_EQ(
+      memcmp(
+          item.getDeviceOs().data(),
+          foundItem->getDeviceOs().data(),
+          item.getDeviceOs().size()),
+      0);
+  database::DatabaseManager::getInstance().removeSessionItem(
+      item.getSessionID());
+}
