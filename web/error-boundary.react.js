e29ed1893e47dbbf5a817b0741ad90f9d6644716
Benjamin Schachter <benschac@gmail.com>
2022-04-21 14:00:09 -0400

    [web] [feat/chore] [ENG-813] add error boundary to the app
    Summary: add error boundary to the web app, when the project crashes, end user gets a message.
    
    Test Plan: This is just a stripped down version of the `error-boundry.react.js` file in native
    
    Reviewers: @atul, @palys-swm, @def-au1t, @yayabosh
    
    Subscribers:

---
+++ web/error-boundary.react.js
@@ -0,0 +1,48 @@
+// @flow
+
+import * as React from 'react';
+
+import type { ErrorInfo, ErrorData } from 'lib/types/report-types';
+
+import css from './error-boundary.css';
+
+type Props = {
+  children: React.Node,
+};
+
+type State = {
+  errorData: $ReadOnlyArray<ErrorData>,
+  showError: boolean,
+};
+
+class ErrorBoundary extends React.PureComponent<Props, State> {
+  state: State = {
+    errorData: [],
+    showError: false,
+  };
+
+  componentDidCatch(error: Error, info: ErrorInfo) {
+    this.setState(prevState => ({
+      errorData: [...prevState.errorData, { error, info }],
+    }));
+  }
+
+  reportError(error: Error) {
+    this.setState(prevState => ({
+      errorData: [...prevState.errorData, { error }],
+    }));
+  }
+
+  render(): React.Node {
+    if (this.state.errorData.length > 0) {
+      return (
+        <div className={css.container}>
+          <h3>Something has gone wrong, please reload the page</h3>
+        </div>
+      );
+    }
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
