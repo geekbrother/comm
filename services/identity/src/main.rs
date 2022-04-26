3c8647c226de95141d2e413d771ebd19a472e5cb
Varun Dhananjaya <varun.dhananjaya@gmail.com>
2022-04-25 23:37:40 -0400

    [Identity] Add tracing to the Identity service
    Summary: Tracing provides structured logging and diagnostics info. It's especially helpful for asynchronous programs where log lines for various tasks can get intermixed.
    
    Test Plan: Checked that detailed error logs appeared in my terminal
    
    Reviewers: jimpo, atul, karol-bisztyga, palys-swm, geekbrother
    
    Subscribers: ashoat, Adrian, yayabosh
    
    Differential Revision: https://phabricator.ashoat.com/D3852

--- services/identity/src/main.rs
+++ services/identity/src/main.rs
@@ -1,4 +1,5 @@
 use tonic::transport::Server;
+use tracing_subscriber::FmtSubscriber;
 
 mod service;
 use service::{IdentityServiceServer, MyIdentityService};
@@ -7,12 +8,15 @@ const IDENTITY_SERVICE_SOCKET_ADDR: &str = "[::]:50051";
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+  let subscriber = FmtSubscriber::new();
+  let _ =
+    tracing::subscriber::set_global_default(subscriber).expect("Setting tracing default failed");
   let addr = IDENTITY_SERVICE_SOCKET_ADDR.parse()?;
   let mut identity_service = MyIdentityService::default();
   identity_service
     .config
     .load()
-    .expect("config successfully loaded");
+    .expect("Failed to load config successfully");
 
   Server::builder()
     .add_service(IdentityServiceServer::new(identity_service))
